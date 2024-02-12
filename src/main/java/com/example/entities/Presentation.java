86decb1742f2157ad2673e045ab2d6d5715af76c
clemmory <clementine.mory@gmail.com>
2024-02-12 10:42:24 +0100

    Initial State

---
+++ src/main/java/com/example/entities/Presentation.java
@@ -0,0 +1,10 @@
+package com.example.entities;
+
+public class Presentation {
+
+    private int id;
+    private String name;
+    private String description;
+
+
+}
---
+++ src/main/java/com/example/entities/Product.java
@@ -0,0 +1,11 @@
+package com.example.entities;
+
+public class Product {
+    
+    private int id;
+    private String name;
+    private String description;
+    private int stock;
+    private double price;
+
+}
