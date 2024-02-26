16820b5199b2bdebdb93ff7e0539f2f9215cdb23
clemmory <clementine.mory@gmail.com>
2024-02-26 10:46:49 +0100

    Security files from Victor

--- src/main/java/com/example/security/ConstantesSeguridad.java
+++
@@ -1,7 +0,0 @@
-package com.example.security;
-
-public class ConstantesSeguridad {
-
-    public static final long JWT_EXPIRATION_TOKEN = 300000; //equivalent to 5min 60000 = 1min
-    public static final String JWT_FIRMA = "firma";
-}
